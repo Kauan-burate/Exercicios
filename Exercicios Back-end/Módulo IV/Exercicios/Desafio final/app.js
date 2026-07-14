7325846e2ebe03013a2f275b8a2a0e971fba7cf5
KAUAN BURATE DE SOUZA <kauan_b_souza@estudante.sesisenai.org.br>
2026-07-13 21:31:26 -0300

    Exercicos de ProgreSQL + VScode

--- Exercicios Back-end/Módulo IV/Exercicios/Desafio final/app.js
+++ Exercicios Back-end/Módulo IV/Exercicios/Desafio final/app.js
@@ -8,27 +8,14 @@ const client = new Client({
     host:     'localhost',
     port:     5432,
     user:     'postgres',
-    password: '150783',
+    password: 'root',
     database: 'escola_db'
 });
 
 
-function menu (){
-    console.log("══════════════════════════════════");
-    console.log("   🎓 SISTEMA DE TURMA 3DS");
-    console.log("══════════════════════════════════");
-    console.log("1 - Ver todos os alunos");
-    console.log("2 - Ver situação da turma");
-    console.log("3 - Cadastrar aluno");
-    console.log("4 - Lançar nota");
-    console.log("5 - Remover aluno");
-    console.log("0 - Sair");
-    console.log("══════════════════════════════════");
-}
 
  async function VizualizarAlunos(){
     try{
-
         const nomeNotaAlunos   = await client.query(`SELECT nome, turma, nota FROM alunos`);
 
         console.log("DADOS DOS ALUNOS: ");
@@ -38,9 +25,6 @@ function menu (){
     catch(error){
         console.log(error.message);
     }
-    finally{
-        await client.end();
-    }
 
 }
 async function VizualizarTurma(){
@@ -48,59 +32,133 @@ async function VizualizarTurma(){
 }
 async function CadastrarAluno(){
 
+   
 }
+
 async function LancarNota(){
+
+    try {
+        
+        const listar = `
+        SELECT id, nome, nota FROM alunos
+        `;
+    
+        const listarAlunos = await client.query(listar);
+        console.log("=========================");
+        console.log("ALUNOS CADASTRADOS");
+        console.log("=========================");
+        console.table(listarAlunos.rows);
+    
+    
+    //===============================================//================================================//    
+    
+    
+        console.log("============================");
+        console.log("ATUALIZAR DADOS DOS ALUNOS");
+        console.log("============================");
+    
+        const novoId    = Number(prompt("Digite o ID do aluno que deseja alterar a nota: "));
+        const novaNota  = Number(prompt("Digite a nova nota: "));
+    
+        if(novoId < 0 || novoId > 8){
+            console.log("ERRO: Digite um id existente!");
+        } else if( novaNota < 0 || novaNota > 10){
+            console.log("ERRO: Digite uma Nota válida!")
+        } else{
+            const dadosParaAlteracao = `
+                UPDATE alunos 
+                SET nota = $1
+                WHERE id = $2
+                RETURNING *`;
+    
+            const dados = [novaNota, novoId];
+            const resultadoAlteracao = await client.query(dadosParaAlteracao, dados);
+    
+    
+            let situacao;
+    
+            if(novaNota >= 7){
+                situacao = "Aprovado ✅";
+            } else if(novaNota >= 5){
+                situacao = "Recuperação ⚠️";
+            } else{
+                situacao = "Reprovado ❌";
+            }
+    
+            console.table(resultadoAlteracao.rows);
+            console.log("Dados atualizados com sucesso!");
+    
+            console.log("\n==========================");
+            console.log("✔ Nota atualizada com sucesso!");
+            console.log(`Nova Nota: ${novaNota}`);
+            console.log(`Situação: ${situacao}`);
+            console.log("==========================");
+    
+        }
+    
+      
+    } catch (error) {
+        console.log(error.message);
+    
+    }
+
+
+
+
 
 }
 async function RemoverAluno(){
 
 }
 
-await client.end();
+async function menu (){
 
-async function main(){
-    let sistemaAtivo = true;
-    while(sistemaAtivo){
-        menu();
-        const comando = Number(prompt("Digite um comando: "));
-        if(comando < 0 || comando > 5){
-            console.log("Comando inválido!");
-        } else{
+    try {
+        await client.connect();
+
+        let sistemaAtivo = true;
+
+        while(sistemaAtivo){
+            console.log("══════════════════════════════════");
+            console.log("   🎓 SISTEMA DE TURMA 3DS");
+            console.log("══════════════════════════════════");
+            console.log("1 - Ver todos os alunos");
+            console.log("2 - Ver situação da turma");
+            console.log("3 - Cadastrar aluno");
+            console.log("4 - Lançar nota");
+            console.log("5 - Remover aluno");
+            console.log("0 - Sair");
+            console.log("══════════════════════════════════");
+
+            const comando = Number(prompt("Digite um Comando: "));
             switch (comando) {
-                case 1:
-                    VizualizarAlunos();
-                    break;
+                case 1: await VizualizarAlunos();   break;
+                case 2: await VizualizarTurma();    break;
+                case 3: await CadastrarAluno();     break;
+                case 4: await LancarNota();         break;
+                case 5: await RemoverAluno();       break;
+                case 0: sistemaAtivo = false; console.log("Saindo do sistema...."); break;
+                default: console.log("Comando inválido!");
+            }
 
-                case 2:
-                    VizualizarTurma();
-                    break;
-
-                case 3:
-                    CadastrarAluno();
-                    break;
-
-                case 4:
-                    LancarNota();
-                    break;
-
-                case 5:
-                    RemoverAluno();
-                    break;
-
-                case 0:
-                    sistemaAtivo = false;
-                    break;
-
-                default:
-                    // código caso nenhum case seja atendido
-                }
 
         }
-        main();
+  
+        } catch (error) {
+            console.log(error.message);
+        }
+        finally{
+            await client.end();
 
-    }
-
+        }
 
 }
+await menu();
+
+    
+    
+
+
+
 
 
